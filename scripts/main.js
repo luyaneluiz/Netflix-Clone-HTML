0f6630eb08a214e219861d9e02b31fb6feca29ac
luyane luiz <luyane.luiz.ll@gmail.com>
2022-06-08 14:44:34 -0300

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 5,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    300: {
+      items: 2,
+    },
+    600: {
+      items: 3,
+    },
+    900: {
+      items: 4,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
